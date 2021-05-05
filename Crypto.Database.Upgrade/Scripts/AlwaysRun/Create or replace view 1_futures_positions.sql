a34d5134be6239d2d625d03049791e3dd1014dc1
Lambda125 <927405+lambda125@users.noreply.github.com>
2021-05-05 23:21:55 +1000

    Updating view to use latest state machine for signals, commands, positions, orders

--- Crypto.Database.Upgrade/Scripts/AlwaysRun/Create or replace view 1_futures_positions.sql
+++ Crypto.Database.Upgrade/Scripts/AlwaysRun/Create or replace view 1_futures_positions.sql
@@ -1,14 +1,19 @@
 DROP VIEW IF EXISTS public.futures_positions CASCADE;
 
-CREATE OR REPLACE VIEW public.futures_positions
-AS SELECT fs2.signal_id,
+SELECT fs2.signal_id,
     fs2.symbol,
     fs2.position_type,
     fs2.exchange_id,
     fs3.strategy_pair_name,
-    CASE
-        WHEN COALESCE(buy.executed_qty, 0) = COALESCE(sell.executed_qty, 0) AND COALESCE(buy.executed_qty, 0) > 0 AND (COALESCE(buy.pending_qty, 0) + COALESCE(sell.pending_qty, 0)) = 0 THEN 'INACTIVE'
-        ELSE 'ACTIVE' -- bugs in the code could cause signals to stay active - even when nothing is happening. That's ok for now.
+    case
+    	when COALESCE(open_command.status, '') = 'CREATED' and coalesce (close_command.status, '') = '' then 'CREATED'
+    	when COALESCE(open_command.status, '') = 'EXPIRED' and coalesce (close_command.status, '') = '' then 'EXPIRED'
+    	when COALESCE(open_command.status, '') = 'SUCCESS' and coalesce (close_command.status, '') IN ('', 'CREATED', 'EXPIRED', 'FAILED') and (COALESCE(buy.executed_qty, 0) > 0 OR COALESCE(sell.executed_qty, 0) > 0) then 'ACTIVE'
+    	when COALESCE(open_command.status, '') = 'FAILED'  and coalesce (close_command.status, '') = '' and (COALESCE(buy.executed_qty, 0) + COALESCE(sell.executed_qty, 0) = 0) then 'ENDED'
+        WHEN COALESCE(buy.executed_qty, 0) = COALESCE(sell.executed_qty, 0) AND COALESCE(buy.executed_qty, 0) > 0 AND (COALESCE(buy.pending_qty, 0) + COALESCE(sell.pending_qty, 0)) = 0 
+        		and COALESCE(open_command.status, '') = 'SUCCESS' and COALESCE(close_command.status, '') = 'SUCCESS'
+        	THEN 'ENDED'
+        ELSE 'UNKNOWN' -- CONCAT('UNKNOWN (open: ', open_command.status, ', close: ', close_command.status, ')') -- bugs in the code could cause signals to stay active - we need to alert on this
     END AS signal_status,
     CASE
         WHEN COALESCE(buy.executed_qty, 0) = COALESCE(sell.executed_qty, 0) AND COALESCE(buy.executed_qty, 0) > 0 AND (COALESCE(buy.pending_qty, 0) + COALESCE(sell.pending_qty, 0)) = 0 THEN 'CLOSED'
@@ -17,7 +22,8 @@ AS SELECT fs2.signal_id,
         WHEN fs2.position_type = 'SHORT' AND COALESCE(buy.pending_qty, 0) > 0 THEN 'PENDING_CLOSE'
         WHEN fs2.position_type = 'SHORT' AND COALESCE(sell.pending_qty, 0) > 0 THEN 'PENDING_OPEN'
         WHEN (COALESCE(buy.executed_qty, 0) + COALESCE(sell.executed_qty, 0)) = 0 AND (COALESCE(buy.pending_qty, 0) + COALESCE(sell.pending_qty, 0)) >= 0 THEN 'NOT_YET_OPEN'
-        ELSE 'OPEN'
+        WHEN (COALESCE(buy.executed_qty, 0) + COALESCE(sell.executed_qty, 0)) > 0 THEN 'OPEN'
+        ELSE 'UNKOWN'
     END AS position_status,
     COALESCE(buy.executed_qty, 0) AS executed_buy_qty,
     COALESCE(buy.pending_qty, 0) AS pending_buy_qty,
@@ -40,6 +46,18 @@ AS SELECT fs2.signal_id,
         ELSE buy.last_order_time
     END AS exit_time
    FROM futures_signal fs2
+   	 LEFT JOIN (SELECT max(id) AS id, signal_id, status, signal_action, count(signal_id) AS commandCount 
+   	 		    FROM futures_signal_command 
+   	 		    WHERE signal_action = 'OPEN'
+			    GROUP BY signal_id, status, signal_action
+			    ORDER BY id DESC LIMIT 1
+   	 	) open_command ON open_command.signal_id = fs2.signal_id
+   	 LEFT JOIN (SELECT max(id) AS id, signal_id, status, signal_action, count(signal_id) AS commandCount 
+   	 		    FROM futures_signal_command 
+   	 		    WHERE signal_action = 'CLOSE'
+			    GROUP BY signal_id, status, signal_action
+			    ORDER BY id DESC LIMIT 1
+   	 	) close_command ON close_command.signal_id = fs2.signal_id
      LEFT JOIN ( SELECT signal_order_summary.signal_id,
             signal_order_summary.order_side,
             sum(signal_order_summary.executed_qty) AS executed_qty,
