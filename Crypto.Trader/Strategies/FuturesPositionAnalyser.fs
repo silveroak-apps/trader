aac134b241cac908eaf8f9fcd41c5382a5c07c50
gumbaba <gumbaba@outlook.com>
2021-06-10 01:24:33 +1000

    tweaked the stoploss desabled breakeven and stoploss -2 and tp 4

--- Crypto.Trader/Strategies/FuturesPositionAnalyser.fs
+++ Crypto.Trader/Strategies/FuturesPositionAnalyser.fs
@@ -190,7 +190,7 @@ let private calculateStopLoss (position: PositionAnalysis) (gainOpt: decimal opt
             expectedCostsForTradeCycle (breakEvenStopLoss) = 1.1%
     *)
 
-    let trailingTakeProfitLevel = 1.5M * decimal position.Leverage // % - TODO move to config
+    let trailingTakeProfitLevel = 1.2M * decimal position.Leverage // % - TODO move to config
     let trailingDistance = 0.7M * decimal position.Leverage // % - TODO move to config
 
     let breakEvenTrigger = 0.3M * decimal position.Leverage // % - TODO move to config
