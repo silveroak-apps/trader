2e77da9fa79cefafc2c1614ce12876c4c7eaa729
Krishna Singampalli <singampk@gmail.com>
2022-02-04 18:37:55 +1100

    removind dependences and old views

--- Crypto.Database.Upgrade/Scripts/Script 0047 - Cleanup and rename signals tables.sql
+++ Crypto.Database.Upgrade/Scripts/Script 0047 - Cleanup and rename signals tables.sql
@@ -1,6 +1,12 @@
 ALTER TABLE futures_signal RENAME TO signal;
 ALTER TABLE futures_signal_command RENAME TO signal_command;
 
+ALTER TABLE sell_analysis DROP CONSTRAINT fk_sell_analysis2_signal;
+DROP view webview
+DROP view overview_buy_analysis
+DROP view overview_sell_analysis
+DROP view overview
+DROP view summary
 drop table positive_signal;
 drop table positive_signal_excel;
 ALTER TABLE signal  ADD COLUMN strategy_pair_id bigserial;
@@ -18,3 +24,4 @@ ALTER TABLE signal_command
 ADD CONSTRAINT fk_strategy_condition
 FOREIGN KEY (strategy_condition_id) 
 REFERENCES strategy_conditions (id);
+
